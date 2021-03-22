0a5a89af55ca2ea1500025243d644a1bfd46b47c
Matthew Bolding <boldingmatthewd@gmail.com>
2021-03-22 13:56:08 -0500

    enhanced details in  for system checks

--- file-system-checks.sh
+++ file-system-checks.sh
@@ -3,7 +3,11 @@ DEV=
 DIR=/plex
 FILE=disk-checks-`date +\%Y-\%m-\%d-%H:%M:%S`.log
 
-systemctl stop plexmediaserver; systemctl status plexmediaserver >> $FILE
-umount $DEV; lsblk | grep $DEV >> $FILE
+echo "System Diagnostics started at `date +\%Y-\%m-\%d-%H:%M:%S`\\n" >> $FILE
+
+systemctl stop plexmediaserver && echo "Plex Media Server stopped...\\n" >> $FILE
+umount $DEV && echo "$DEV unmounted from $DIR\\n"; lsblk | grep $DEV >> $FILE
 fsck -CVy $DEV >> $FILE && mount $DEV $DIR; lsblk | grep $DEV >> $FILE
-systemctl start plexmediaserver; systemctl status plexmediaserver >> $FILE
+systemctl start plexmediaserver && echo "Plex Media Server restarted...\\n" >> $FILE
+
+echo "System Diagnostics completed at `date +\%Y-\%m-\%d-%H:%M:%S`" >> $FILE
